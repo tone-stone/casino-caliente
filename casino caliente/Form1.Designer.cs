820327f7adad702bdca8b8d884484a178a2611b7
Tone <ing.antoniovilla@outlook.com>
2022-09-20 02:22:03 -0700

    Agregar archivos de proyecto.

---
+++ casino caliente/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace casino_caliente
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Stencil", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(32, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 114);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "0";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Stencil", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(166, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 114);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("Stencil", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(291, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 114);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(170, 233);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "jugar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.ForestGreen;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(421, 331);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Traga Monedas";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ casino caliente/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace casino_caliente
+{
+    public partial class Form1 : Form
+    {
+        int i, j, k, max1, max2, max3;
+        string digito1, digito2, digito3;
+        Random numeroAleatorio1 = new Random(), numeroAleatorio2 = new Random(), numeroAleatorio3 = new Random();
+        //Creo 3 objetos de la clase ''random'' (aleatoio) para generar numeros al azar.
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            i = 0;
+            j = 0;
+            k = 0;
+            max1 = numeroAleatorio1.Next(10, 100);
+            max2 = numeroAleatorio1.Next(10, 100) + numeroAleatorio2.Next(10, 100);
+            max3 = numeroAleatorio1.Next(10, 100) + numeroAleatorio2.Next(10, 100) + numeroAleatorio3.Next(10, 100);
+            while (k <= max3)
+            {
+                if (i <= max1)
+                {
+                    digito1 = Convert.ToString(i);
+                }
+                if (j <= max2)
+                {
+                    digito2 = Convert.ToString(j);
+                }
+                digito3 = Convert.ToString(k);
+                label1.Text = digito1.Substring(digito1.Length - 1);
+                label2.Text = digito2.Substring(digito2.Length - 1);
+                label3.Text = digito3.Substring(digito3.Length - 1);
+                label1.Refresh();
+                label2.Refresh();
+                label3.Refresh();
+                i++;
+                j++;
+                k++;
+                Thread.Sleep(10);
+            }
+            if (label1.Text == label2.Text && label2.Text == label3.Text)
+                MessageBox.Show("Ganaste $1000,000 dlls !!!", "FELICIDADES!!!");
+            else
+                MessageBox.Show("Suerte para la proxima!!!", "LASTIMA!!!");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
